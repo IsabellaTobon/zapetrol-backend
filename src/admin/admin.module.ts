13fc8ed8fd99c4120405dbb829ea33a96ec83685
Isabella T. <89875811+IsabellaTobon@users.noreply.github.com>
2025-11-15 02:10:18 +0100

    Update admin.module.ts

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { AuthModule } from '../auth/auth.module';
 import { AdminUsersController } from './admin-users.controller';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: config.get('JWT_EXPIRES') },
+      }),
+    }),
+  ],
   controllers: [AdminUsersController],
 })
 export class AdminModule {}
